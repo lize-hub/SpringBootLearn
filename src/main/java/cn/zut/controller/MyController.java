1b7e97f83551842748669738dacfd4c4cfa3c1dc
lize <2412569527@qq.com>
2021-10-28 10:12:26 +0800

    one

---
+++ src/main/java/cn/zut/controller/MyController.java
@@ -0,0 +1,10 @@
+package cn.zut.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public String getName(){
+        return "wanngwu";
+    }
+}
